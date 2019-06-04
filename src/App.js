5460cf6271f30be70fb0581a59ad326a72ef28f0
pompucha <benavidesga@gmail.com>
2019-06-04 09:34:44 -0600

    building out Add.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      groceries: [
+        { id:1, name: "ramen", complete: true, },
+        { id:2, name: "OJ", complete: true, },
+        { id:3, name: "chickens", complete: false, },
+        { id:4, name: "cow", complete: false, },
+        { id:5, name: "tortillas", complete: true, },
+      ]
+    };
+
+    this.addItem = this.addItem.bind(this);
+  }
+
+  getUniqueId = () => {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+   }
+ 
+   addItem(name) {
+       const todo = { name, id: this.getUniqueId(), complete: false };
+       this.setState({ todos: [todo, ...this.state.todos], });
+   };
+ 
+   handleClick = (id) => {
+     this.setState({
+       todos: this.state.todos.map( todo => {
+           if (todo.id === id) {
+             return {
+               ...todo,
+               complete: !todo.complete,
+             }
+           }
+           return todo;
+       })
+     })
+   }
+ 
+   render () {
+     return (
+       <div>
+         <TodoForm addItem={this.addItem} />
+         <List name="Todo List" items={this.state.todos} todoClick={this.handleClick} />
+       </div>
+     )
+   }
+
+
+
+
+
+
+
 }
-
 export default App;
